89e5713d8a257743b8c4057e18d875b35128c2b4
niracler <1026037967@qq.com>
2017-09-20 19:33:24 +0800

    恩，　我先来初始化一下

---
+++ myStack/MyStack.cpp
@@ -0,0 +1,5 @@
+//
+// Created by niracler on 17-9-20.
+//
+
+#include "MyStack.h"
---
+++ myStack/MyStack.h
@@ -0,0 +1,8 @@
+//
+// Created by niracler on 17-9-20.
+//
+
+#ifndef DATASTRUCTURE_MYSTACK_H
+#define DATASTRUCTURE_MYSTACK_H
+
+#endif //DATASTRUCTURE_MYSTACK_H
