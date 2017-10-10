7a2e265732fa150ddf6f9ff38a6a5848d2690555
niracler <1026037967@qq.com>
2017-10-10 11:10:59 +0800

    这里要用bfs算法，真是厉害了

--- homework/chapter03/homework3_3.cpp
+++ homework/chapter03/homework3_3.cpp
@@ -5,3 +5,14 @@
 //一矩形阵列由数字0到9组成，数字1到9代表细胞，细胞的定义为沿细胞数字上下左右还是相同数字则为同一细胞，如:阵列
 //scholat.com
 // 有5个细胞，编写程序求给定矩形阵列的细胞个数
+
+#include <iostream>
+
+using namespace std;
+
+int mainbfs(void)
+{
+
+
+	return 0;
+}
