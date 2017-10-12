087191c119fc0008dc76d47c8d0580f7f2328288
niracler <1026037967@qq.com>
2017-10-12 17:16:19 +0800

    又一失败品

--- homework/chapter03/homework3_3.cpp
+++ homework/chapter03/homework3_3.cpp
@@ -10,9 +10,36 @@
 
 using namespace std;
 
-int mainbfs(void)
+//棋盘格
+struct Item
 {
+	int num; //细胞号数
+	bool flag;//是否走过,走过就插false旗
+};
 
+int main(void)
+{
+	Item member[11][11];
+
+	for (int i = 1; i <= 10; ++i)
+	{
+		for (int j = 1; j <= 10; ++j)
+		{
+			cin >> member[i][j].num;
+		}
+	}
+
+	for (int i = 1; i <= 10; ++i)
+	{
+		for (int j = 1; j <= 10; ++j)
+		{
+			cout << member[i][j].num << " ";
+		}
+
+		cout << endl;
+	}
+
+	cout << "bfs什么的, 去你的!!!!!!" << endl;
 
 	return 0;
 }
