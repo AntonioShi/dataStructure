00eabf3a9093fd442417ddc8a3a7ab29d6c3f966
niracler <1026037967@qq.com>
2017-10-20 21:21:04 +0800

    更正

--- homework/chapter02/homework1_1.c
+++ homework/chapter02/homework1_1.c
@@ -10,17 +10,17 @@
 #include "../../myList/MyList.h"
 #include <malloc.h>
 
-void MyAdd(SLNode * head, DataType a , DataType newData);//在单链表中数据域值为a的结点之后，　插入一个新结点
+void MyAdd(SLNode *head, DataType a, DataType newData);//在单链表中数据域值为a的结点之后，　插入一个新结点
 
-int main0918(void)
+int main201710202120(void)
 {
     int i;                //控制循环
-    SLNode * head = NULL;         //定义头指针
+    SLNode *head = NULL;         //定义头指针
     ListInitiate(&head);   //初始化链表
 
     for (i = 0; i < 10; ++i)
     {
-        MyAdd(head, 1, i);
+        MyAdd(head, 5, i);
     }
 
     ListPrint(head);
@@ -29,19 +29,30 @@ int main0918(void)
 }
 
 //在单链表中数据域值为a的结点之后，　插入一个新结点
-void MyAdd(SLNode * head, DataType a , DataType newData)
+void MyAdd(SLNode *head, DataType a, DataType newData)
 {
-    SLNode * p = head;
-    SLNode * q = NULL;
+    SLNode *p = head;
+    SLNode *q = NULL;
 
-    //为了找到要插入的地方,我的天，这里已经是ｎｕｌｌ
-    for (; p->next != NULL && p->data == a ; p = p->next);
+    //为了找到要插入的地方,修改前
+    //for (; p->next != NULL && p->data == a ; p = p->next);
+
+    //修改后,看来，判断无法避免，
+    for (; p != NULL && p->data != a; q = p,p = p->next);
 
     //挂上newData
-    q = p;
-    p = p->next;
-    q->next = (SLNode *)malloc(sizeof(SLNode));
-    q->next->data = newData;
-    q->next->next = p;
+    if (p != NULL)//找到
+    {
+        q = p->next;//记住下一个
+        p->next = (SLNode *) malloc(sizeof(SLNode));//申请新空间
+        p->next->data = newData;//分配值
+        p->next->next = q;//
+    }
+    else//找不到
+    {
+        q->next = (SLNode *) malloc(sizeof(SLNode));
+        q->next->data = newData;
+        q->next->next = p;
+    }
 }
 
--- homework/chapter03/homework3_3.cpp
+++ homework/chapter03/homework3_3.cpp
@@ -6,53 +6,117 @@
 //scholat.com
 // 有5个细胞，编写程序求给定矩形阵列的细胞个数
 
+//当然，这个是老师的辅助代码
+#include <queue>
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
+
+//两个数组依次结合代表四个方向
+void bfs(int **area, int x, int y, int maxsize,int cell);
 
 //矩阵类
 class Matrix
 {
 private:
-	int **Item;
-	int m, n;
+    int **area;
+    int maxsize;//图像大小（二维数组阶数）
+    int type;//细胞种类数
 public:
-	Matrix(int m, int n);
-	friend istream &operator>>(istream &in, Matrix &mat);
-	friend ostream &operator<<(ostream &out, Matrix &mat);
+    Matrix(int n, int type);
+
+    friend int count(Matrix &mat);         //一个数细胞数目的函数
+    friend ostream &operator<<(ostream &out, Matrix &mat);
 };
 
-int main101320(void)
+int main201710202020(void)
 {
-	Matrix mat(10, 10);
+    Matrix mat(5, 2);
 
-	cout << "bfs什么的, 去你的!!!!!!" << endl;
+    cout << "\n好，下面这里是培养皿:" << endl;
+    cout << mat << endl;
+    cout << "细胞数目：" << count(mat) << endl;
 
-	return 0;
+    return 0;
 }
 
-Matrix::Matrix(int m, int n)
+Matrix::Matrix(int maxsize, int type)
 {
-	Item = new int *[m];
+    this->maxsize = maxsize;
+    this->type = type;
 
-	for (int i = 0; i < m; ++i)
-	{
-		Item[m] = new int[n];
-	}
+    //随机生成显微镜图像
+    area = new int *[maxsize];
+    srand(time(NULL));
+
+    for (int i = 0; i < maxsize; i++)
+    {
+        area[i] = new int[maxsize];
+    }
+
+    for (int i = 0; i < maxsize; i++)
+    {
+        for (int j = 0; j < maxsize; j++)
+        {
+            area[i][j] = rand() % (this->type + 1);
+        }
+    }
 }
 
-//输入函数重载
-istream &operator>>(istream &in, Matrix &mat)
-{
-
-
-	return in;
-}
 
 //输出函数重载
 ostream &operator<<(ostream &out, Matrix &mat)
 {
+    for (int i = 0; i < mat.maxsize; i++)
+    {
+        for (int j = 0; j < mat.maxsize; j++)
+        {
+            out << setw(4) << mat.area[i][j];
+        }
+        out << endl;
+    }
 
+    return out;
+}
 
-	return out;
+//一个数细胞数目的函数
+int count(Matrix &mat)
+{
+    int num = 0;
+    int cell;//细胞数字
+
+    //接下来就一个一个地找，为了保证每次都有四个方向,所以从1开始
+    for (int i = 0; i < mat.maxsize; i++)
+    {
+        for (int j = 0; j < mat.maxsize; j++)
+        {
+            //找到细胞的边角
+            if (mat.area[i][j] != 0)
+            {
+                cell = mat.area[i][j];
+                mat.area[i][j] = 0;//千万不能漏
+                bfs(mat.area, i, j, mat.maxsize, cell);
+                num++;
+            }
+        }
+    }
+
+    return num;
+}
+
+void bfs(int **area, int x, int y, int maxsize, int cell)
+{
+    int x1[4] = {1, 0, -1, 0}, y1[4] = {0, 1, 0, -1};//利用简单数组表示方向；
+
+    //依次找到四个方向
+    for (int i = 0; i < 4; i++)
+    {
+        if (x + x1[i] >= 0 && x + x1[i] < maxsize && y + y1[i] >= 0 && y + y1[i] < maxsize/*排除越界的可能*/
+            && area[x + x1[i]][y + y1[i]] == cell/*确认是否是细胞数字*/)
+        {
+            area[x + x1[i]][y + y1[i]] = 0; //将找到细胞数字置为0防止重复
+            bfs(area, x + x1[i], y + y1[i], maxsize, cell);//搜索下一个点
+        }
+    }
 }
