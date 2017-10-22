1963c4d82e4e7d7d9a599a851a510b0ec7fa5258
niracler <1026037967@qq.com>
2017-10-22 15:03:09 +0800

    这是最开始的方勇的版本

---
+++ otherPeople/classmate/方勇/main.cpp
@@ -0,0 +1,168 @@
+#include "stdio.h"
+#include "malloc.h"
+#include "stdlib.h"
+
+typedef struct node
+{
+    struct node *next;
+    double xNum;
+    int zNum;
+} Node;
+
+void ListInitiate(Node **head)
+{
+    *head = (Node *) malloc(sizeof(Node));
+    (*head)->next = NULL;
+}
+
+int ListLength(Node *head)
+{
+    Node *p = head;
+    int size = 0;
+    while (p->next != NULL)
+    {
+        p = p->next;
+        size++;
+    }
+    return size;
+}
+
+int ListInsert(Node *head, int i, double x, int z)
+{
+    Node *p, *q;
+    int j;
+    p = head;
+    j = -1;
+    while (p->next != NULL && j < i - 1)
+    {
+        p = p->next;
+        j++;
+    }
+    if (j != i - 1)
+    {
+        printf("插入的位置参数有误!");
+        return 0;
+    }
+
+    q = (Node *) malloc(sizeof(Node));
+    q->xNum = x;
+    q->zNum = z;
+    q->next = p->next;
+    p->next = q;
+    return 1;
+}
+
+int ListDelete(Node *head, int i, double *x, int *z)
+{
+    Node *p, *s;
+    int j;
+    p = head;
+    j = -1;
+    while (p->next != NULL && p->next->next != NULL && j < i - 1)
+    {
+        p = p->next;
+        j++;
+    }
+    if (j != i - 1)
+    {
+        printf("插入元素位置参数有误!");
+        return 0;
+    }
+
+    s = p->next;
+    *x = s->xNum;
+    *z = s->zNum;
+    p->next = p->next->next;
+    free(s);
+    return 1;
+}
+
+int ListGet(Node *head, int i, double *x, int *z)
+{
+    Node *p;
+    int j;
+    p = head;
+    j = -1;
+    while (p->next != NULL && j < i)
+    {
+        p = p->next;
+        j++;
+    }
+    if (j != i)
+    {
+        printf("取元素位置参数有误!");
+        return 0;
+    }
+
+    *x = p->xNum;
+    *z = p->zNum;
+    return 1;
+}
+
+void Input(Node *head)
+{
+    double x;
+    int z, n = 1;
+    while (n)
+    {
+        scanf("%lf%d", &x, &z);
+        if (z == 0)
+            n = 0;
+        else
+        {
+            ListInsert(head, ListLength(head), x, z);
+        }
+    }
+}
+
+void Destory(Node **head)
+{
+    Node *p, *p1;
+    p = *head;
+    while (p != NULL)
+    {
+        p1 = p;
+        p = p->next;
+        free(p1);
+    }
+    *head = NULL;
+}
+
+Node *sort(Node *head)
+{
+    Node *p = NULL, *q = NULL, *s = NULL;
+    for (p = head; p->next != NULL; p = p->next)
+    {
+        for (q = p->next; q != NULL; q = q->next)
+        {
+            if (p->zNum == q->zNum)
+            {
+                p->xNum += q->xNum;
+                s = q;
+                q = q->next;
+                free(s);
+            }
+        }
+    }
+
+    return head;
+}
+
+int main(void)
+{
+    int i, z;
+    double x;
+    Node *head1, *head2;
+    ListInitiate(&head1);
+    ListInitiate(&head2);
+    Input(head1);
+    sort(head1);
+    printf("%d\n", ListLength(head1));
+    for (i = 0; i < ListLength(head1); i++)
+    {
+        ListGet(head1, i, &x, &z);
+        printf("%lf    %d\n", x, z);
+    }
+    //system("pause");
+    return 0;
+} 
